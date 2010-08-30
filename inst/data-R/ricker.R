dd13ed81601776607255c5ede9ea00e9c4304f33
Aaron A. King <kingaa@umich.edu>
2010-08-30 00:00:00 +0000

    version 0.32-5

---
+++ inst/data-R/ricker.R
@@ -0,0 +1,27 @@
+require(pomp)
+
+simulate(
+         pomp(
+              data=data.frame(time=seq(0,50,by=1),y=NA),
+              times="time",
+              t0=0,
+              rprocess=discrete.time.sim(
+                step.fun="_ricker_simulator"
+                ),
+              rmeasure="_ricker_poisson_rmeasure",
+              dmeasure="_ricker_poisson_dmeasure",
+              skeleton.map="_ricker_skeleton",
+              paramnames=c("log.r","log.sigma","log.phi"),
+              statenames=c("N","e"),
+              obsnames=c("y")
+              ),
+         params=c(
+           log.r=3.8,
+           log.sigma=log(0.3),
+           log.phi=log(10),
+           N.0=7,
+           e.0=0
+           ),
+         nsim=1,
+         seed=73691676L
+         ) -> ricker
