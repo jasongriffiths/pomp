6950a7b82344ce165b1c81c48d925d9c6b4013e0
Aaron A. King <kingaa@umich.edu>
2011-04-20 00:00:00 +0000

    version 0.36-4

--- inst/data-R/make.R
+++
@@ -1,6 +0,0 @@
-examples <- c("euler.sir","gillespie.sir","ou2","rw2","verhulst","dacca","ricker")
-
-for (ex in examples) {
-  source(file=paste(ex,"R",sep="."))
-  save(list=ex,file=paste(ex,"rda",sep="."))
-}
